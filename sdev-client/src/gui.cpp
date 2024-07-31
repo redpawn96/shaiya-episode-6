58118a9e88706f062c1cb9a335b40a41b84dd3e8
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-07-31 01:18:50 -0400

    Remove weapon step hook

--- sdev-client/src/gui.cpp
+++ sdev-client/src/gui.cpp
@@ -3,32 +3,6 @@
 #include <util/util.h>
 #include "include/main.h"
 
-const std::array<uint16_t, 21> g_weapon_step
-{
-    0, 7, 14, 21, 31, 41, 51, 64, 77, 90, 106, 122, 138, 157, 176, 195, 217, 239, 261, 286, 311
-};
-
-int get_weapon_step(uint8_t enchantStep)
-{
-    if (enchantStep >= g_weapon_step.size())
-        return 0;
-
-    return g_weapon_step[enchantStep];
-}
-
-unsigned u0x4B8766 = 0x4B8766;
-void __declspec(naked) naked_0x4B8755()
-{
-    __asm
-    {
-        push eax // step
-        call get_weapon_step
-        add esp,0x4
-
-        jmp u0x4B8766
-    }
-}
-
 unsigned u0x41F9ED = 0x41F9ED;
 unsigned u0x41F9C9 = 0x41F9C9;
 void __declspec(naked) naked_0x41F9C0()
@@ -75,8 +49,6 @@ void __declspec(naked) naked_0x41E2BB()
 
 void hook::gui()
 {
-    // weapon enchant bug
-    util::detour((void*)0x4B8755, naked_0x4B8755, 5);
     // dungeon wings shadow workaround
     util::detour((void*)0x41F9C0, naked_0x41F9C0, 9);
     // evolution bug
